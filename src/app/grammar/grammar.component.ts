a362a29ad1ad9ce21c58e9efcc33aa09527ff163
zjn700 <zjn700-01@yahoo.com>
2021-01-09 06:04:35 -0500

    grid testing

--- src/app/grammar/grammar.component.ts
+++ src/app/grammar/grammar.component.ts
@@ -6,7 +6,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./grammar.component.scss']
 })
 export class GrammarComponent implements OnInit {
-
+vocab = [
+    {"word":"tindog",
+    "meaning": "stand up"},
+    {"word":"pungko",
+    "meaning": "sit down"},
+    {"word":"buligi ko bi",
+    "meaning": "help me please"},
+    {"word":"gai ko bi",
+    "meaning": "give me some, please"},
+    {"word":"tindog",
+    "meaning": "stand up"},
+  ]
   constructor() { }
 
   ngOnInit(): void {
